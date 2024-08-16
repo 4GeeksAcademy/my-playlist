ad3e27cbec46241182e5cd343edd2034b17991bf
Dariet-SP <159501959+Dariet-SP@users.noreply.github.com>
2024-08-16 23:00:56 +0000

    my commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,76 @@
+import { useEffect, useRef, useState } from 'react';
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [songs, setSongs] = useState([]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(0);
+  const ActualSong = useRef();
+
+
+  const SongList = () => {
+    fetch('https://playground.4geeks.com/sound/songs')
+      .then(response => response.json())
+      .then(data => setSongs(data.songs))
+      .catch(error => console.error('Error:', error));
+  }
+
+  useEffect(()=>{
+    SongList()
+  },[]
+  );
+
+  const playSongs = (url) => {
+	ActualSong.current.src = `https://playground.4geeks.com${url}`;   
+	ActualSong.current.play();
+    setIsPlaying(true);
+	console.log(url);
+  };
+
+  const handlePlayPause = () => {
+    if (isPlaying) {
+      ActualSong.current.pause();
+      setIsPlaying(false);
+    } else {
+      ActualSong.current.play();
+      setIsPlaying(true);
+    }
+  };
+
+  const NextSong = () => {
+	setCurrentSong((currentSong) => (currentSong + 1) % songs.length);
+	SongList(songs[currentSong].url);
+  };
+
+  const PreviousSong = () => {
+	setCurrentSong((currentSong) => (currentSong - 1 + songs.length) % songs.length);
+	 SongList(songs[currentSong].url);
+  };
+  
+  return (
+    <div className="container" >
+      <h1>Songs List:</h1>
+      <ul>
+        {songs.map((song) => (
+          <li key={song.id} onClick={() => playSongs(song.url)}>{song.name}</li>
+        ))}
+      </ul>
+      <audio src= "" ref={ActualSong}></audio>
+	  
+	  <button onClick={PreviousSong}>
+		{/* poner el iconito de fontawesome */}
+	  </button>
+
+      <button onClick={handlePlayPause}>
+        {isPlaying ? 'Pause' : 'Play'}
+      </button>
+
+	  <button onClick={NextSong}>
+		{/* poner el iconito de fontawesome */}
+	  </button>
+
+    </div>
+  );
 };
 
 export default Home;
